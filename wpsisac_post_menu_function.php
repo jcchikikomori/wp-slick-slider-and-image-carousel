35a575fb5e4fc04e2e1e25cacf7788107216e6ba
anoopranawat <anoopranawat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 09:22:10 +0000

    design
    git-svn-id: http://plugins.svn.wordpress.org/wp-slick-slider-and-image-carousel/trunk@1295628 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsisac_post_menu_function.php
+++ wpsisac_post_menu_function.php
@@ -1,29 +1,33 @@
 <?php
 add_action('admin_menu', 'register_wpsisac_slider_submenu_page');
 function register_wpsisac_slider_submenu_page() {
-	add_submenu_page( 'edit.php?post_type=slick_slider', 'Slider Designs', 'Slider Designs', 'manage_options', 'wpsisac_slider-submenu-page', 'register_wpsisac_slider_page_callback' );
+	add_submenu_page( 'edit.php?post_type=slick_slider', 'Slider Designs', 'Slider Designs', 'manage_options', 'wpsisac_slider-submenu-page', 'register_wpsisac_slider_page_callback' );
+
 }
-function register_wpsisac_slider_page_callback() {
+function register_wpsisac_slider_page_callback() {
+
 	$result ='<div class="wrap"><div id="icon-tools" class="icon32"></div><h2 style="padding:15px 0">Slider Designs</h2></div>
-				<div class="postdesigns medium-4 columns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-1.jpg"><p><code>[slick-slider design="design-1"]</code></p></div>
-				<div class="postdesigns medium-4 columns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-2.jpg"><p><code>[slick-slider design="design-2"]</code></p></div>
-				<div class="postdesigns medium-4 columns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-3.jpg"><p><code>[slick-slider design="design-3"]</code></p></div>
-				<div class="postdesigns medium-4 columns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-4.jpg"><p><code>[slick-slider design="design-4"]</code></p></div>
-				<div class="postdesigns medium-4 columns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-5.jpg"><p><code>[slick-slider design="design-5"]</code></p></div>
+				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-1.jpg"><p><code>[slick-slider design="design-1"]</code></p></div></div>
+				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-2.jpg"><p><code>[slick-slider design="design-2"]</code></p></div></div>
+				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-3.jpg"><p><code>[slick-slider design="design-3"]</code></p></div></div>
+				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-4.jpg"><p><code>[slick-slider design="design-4"]</code></p></div></div>
+				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-5.jpg"><p><code>[slick-slider design="design-5"]</code></p></div></div>
 				<h2 style="padding:15px 0; clear:both;">Carousel Designs</h2>
-				<div class="postdesigns medium-4 columns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-6.jpg"><p><code>[slick-carousel-slider] OR [slick-carousel-slider centermode="true"] OR [slick-carousel-slider variablewidth="true"]</code></p></div>';
-	echo $result;
-}
+				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/design-6.jpg"><p><code>[slick-carousel-slider] OR [slick-carousel-slider centermode="true"] OR [slick-carousel-slider variablewidth="true"]</code></p></div></div>';
+
+	echo $result;
+
+}
+
 function register_wpsisac_slider_admin_style(){
 	?>
 	<style type="text/css">
-	.column, .columns {-webkit-box-sizing: border-box; 
--moz-box-sizing: border-box;    
-box-sizing: border-box;}
-.postdesigns img{width:100%; height:auto}
+	.postdesigns{-moz-box-shadow: 0 0 5px #ddd;-webkit-box-shadow: 0 0 5px#ddd;box-shadow: 0 0 5px #ddd; background:#fff; padding:10px;  margin-bottom:15px;}
+	.column, .columns {-webkit-box-sizing: border-box;-moz-box-sizing: border-box;  box-sizing: border-box;}
+.postdesigns img{width:100%; height:auto;}
 @media only screen and (min-width: 40.0625em) {  
   .column,
-  .columns {position: relative;padding-left: 0.9375em;padding-right: 0.9375em;float: left; }
+  .columns {position: relative;padding-left:10px;padding-right:10px;float: left; }
   .medium-1 {    width: 8.33333%; }
   .medium-2 {    width: 16.66667%; }
   .medium-3 {    width: 25%; }
@@ -37,6 +41,9 @@ box-sizing: border-box;}
   .medium-11 {    width: 91.66667%; }
   .medium-12 {    width: 100%; } 
    }
-	</style>
-<?php }
-add_action('admin_head', 'register_wpsisac_slider_admin_style');
+	</style>
+
+<?php }
+
+add_action('admin_head', 'register_wpsisac_slider_admin_style');
+
