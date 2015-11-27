b434cec5b1b60a42d4feeeba46d22b17eae9216a
anoopranawat <anoopranawat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 09:06:00 +0000

    fixed wpsisac-slick-carousal bug
    git-svn-id: http://plugins.svn.wordpress.org/wp-slick-slider-and-image-carousel/trunk@1295623 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/wpsisac-carousel-template.php
+++ templates/wpsisac-carousel-template.php
@@ -25,7 +25,7 @@ function get_wpsisac_carousel_slider( $atts, $content = null ){
     {
       breakpoint: 640,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 1,		 dots: false
       }
     },
