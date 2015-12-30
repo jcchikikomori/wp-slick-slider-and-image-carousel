d5bc07b32d37929457f3a7570bc49fc65b3668de
anoopranawat <anoopranawat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-12-30 13:17:42 +0000

    fixed error
    git-svn-id: http://plugins.svn.wordpress.org/wp-slick-slider-and-image-carousel/trunk@1318509 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsisac_post_menu_function.php
+++ wpsisac_post_menu_function.php
@@ -19,7 +19,7 @@ function register_wpsisac_slider_page_callback() {
 				<div class="medium-4 columns"><div class="postdesigns"><h2>Carousel Designs</h2><img  src="'.plugin_dir_url( __FILE__ ).'images/design-6.jpg"><p><code>[slick-carousel-slider] OR [slick-carousel-slider centermode="true"] OR [slick-carousel-slider variablewidth="true"]</code></p></div></div>
 				
 				<div class="medium-12 columns"><h1>Buy Pro Designs of WP Slick Slider and Carousel</h1>
-				<p><a href="" target="_blank"><img  src="'.plugin_dir_url( __FILE__ ).'images/slick-slider.png"></a></p></div>
+				<p><a href="http://wponlinesupport.com/sp_plugin/wp-slick-slider-and-image-carousel/" target="_blank"><img  src="'.plugin_dir_url( __FILE__ ).'images/slick-slider.png"></a></p></div>
 				<div class="medium-12 columns"><h3>Complete shortcode for Slider:</h3><p><code>[slick-slider  design="prodesign-1" category="8" show_content="true" limit="5"
  dots="true" arrows="true" autoplay="true" sliderheight="400" autoplay_interval="5000" speed="1000" effect="false" loop="true"]</code></p></div>
 				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/prodesign-1.jpg"><p><code>[slick-slider design="prodesign-1"]</code></p></div></div>
@@ -33,7 +33,6 @@ function register_wpsisac_slider_page_callback() {
 				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/prodesign-9.jpg"><p><code>[slick-slider design="prodesign-9"]</code></p></div></div>
 				<div class="medium-4 columns"><div class="postdesigns"><img  src="'.plugin_dir_url( __FILE__ ).'images/prodesign-10.jpg"><p><code>[slick-slider design="prodesign-10"]</code></p></div></div>
 				<div class="medium-12 columns">
-				<p><a href="http://wponlinesupport.com/sp_plugin/wp-slick-slider-and-image-carousel/" target="_blank"><img  src="'.plugin_dir_url( __FILE__ ).'images/slick-slider.png"></a></p>
 				<h3>Complete shortcode for Carousel Slider:</h3><p><code>[slick-carousel-slider  design="prodesign-11" category="8" limit="5"
  slidestoshow="4" slidestoscroll="1" dots="true" image_size="large" show_content="true" arrows="true" autoplay="true"  autoplay_interval="5000" speed="1000" centermode="true" variablewidth="true" loop="true"]</code></p></div>
 
